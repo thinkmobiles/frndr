5d11af4f298a150d2c6f8805c9e294beb987a968
Killer57575 <Killer57575@gmail.com>
2015-09-09 12:04:42 +0300

    signIn

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ module.exports = function(db){
     "use strict";
 
     require('./user')(db);
-    //require('./like')(db);
+    require('./like')(db);
     require('./message')(db);
     require('./searchSettings')(db);
 };
